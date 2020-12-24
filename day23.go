a9cc0a84ba2006c10818c2e0eff1f2ff8db92334
christucket <hornandrewm@gmail.com>
2020-12-23 22:38:29 -0600

    day23 part 2 with linked list and cache for pointers

--- day23.go
+++ day23.go
@@ -6,128 +6,143 @@ import (
 	"strconv"
 )
 
-func FindCurrentCupIndex(current_cup int, ring []int) int {
-	for i := range ring {
-		if ring[i] == current_cup {
-			return i
-		}
-	}
-	return -1
+type Node struct {
+	val      int
+	NextNode *Node
 }
 
-func GetNextThreeCups(current_cup_index int, ring []int) []int {
-	next_three_cups := []int{}
-	for _, k := range []int{1, 2, 3} {
-		idx := (current_cup_index + k) % len(ring)
-		next_three_cups = append(next_three_cups, ring[idx])
-		ring[idx] = -1
+var size int
+var cache map[int]*Node
+
+func FindDestinationCup(current_cup *Node, taken_out_cups *Node) *Node {
+	looking_for := current_cup.val - 1
+	if looking_for == 0 {
+		looking_for = size
 	}
+	restart := true
 
-	return next_three_cups
-}
-
-func FindDestinationCup(finding_cup int, ring []int, next_three_cups []int) (int, int) {
-	i := 0
-	if finding_cup == 0 {
-		finding_cup = len(ring)
-	}
-
-	f := true
-	for f {
-		f = false
-		for _, k := range next_three_cups {
-			if finding_cup == k {
-				finding_cup--
-				if finding_cup == 0 {
-					finding_cup = len(ring)
+	for restart {
+		restart = false
+		looking_at := taken_out_cups
+		for range []int{1, 2, 3} {
+			if looking_at.val == looking_for {
+				looking_for--
+				if looking_for == 0 {
+					looking_for = size
 				}
-				f = true
+				restart = true
+				break
 			}
+			looking_at = looking_at.NextNode
 		}
 	}
-	for i < len(ring) {
-		// fmt.Println("trying to find", finding_cup)
-		if ring[i] == finding_cup {
-			return i, finding_cup
-		}
-		i++
-	}
 
-	fmt.Println("couldnt find ", finding_cup, next_three_cups)
-
-	return -1, -1
+	return cache[looking_for]
 }
 
-func PlayCupsGame(ring []int, number_of_moves int) []int {
-	current_cup := ring[0]
+func GetRingString(current_cup *Node, sep string) string {
+	f := ""
+	first := current_cup
+
+	for {
+		f += fmt.Sprintf("%v"+sep, current_cup.val)
+		current_cup = current_cup.NextNode
+
+		if current_cup == first {
+			break
+		}
+	}
+
+	return f[:len(f)-len(sep)]
+}
+
+func PlayCupsGame(ring *Node, number_of_moves int) *Node {
+	current_cup := ring
 
 	move := 1
 	for move <= number_of_moves {
-		current_cup_index := FindCurrentCupIndex(current_cup, ring)
-		// fmt.Println(" -> move [", move, "]   ")
-		// fmt.Println("cups: ", ring, current_cup, current_cup_index)
-		next_three_cups := GetNextThreeCups(current_cup_index, ring)
-		_, destination_cup := FindDestinationCup(current_cup-1, ring, next_three_cups)
+		next_three_cups := current_cup.NextNode
 
-		if destination_cup == 1 {
-			fmt.Println("something after 1", next_three_cups)
-		}
-		// fmt.Println("pick up: ", next_three_cups)
-		// fmt.Println("destnation: ", destination_cup, destination_cup_index)
-		// fmt.Println("")
-		next_ring := []int{}
-		for _, k := range ring {
-			if k != -1 {
-				next_ring = append(next_ring, k)
-			}
-			if k == destination_cup {
-				next_ring = append(next_ring, next_three_cups...)
-			}
-		}
+		// set the nextnode to 3 values after the current cup
+		// takes 3 cups out of the loop
+		current_cup.NextNode = current_cup.NextNode.NextNode.NextNode.NextNode
+		// find the destination cup, has to use three cups to avoid using those values
+		destination_cup := FindDestinationCup(current_cup, next_three_cups)
 
-		ring = next_ring
-		current_cup_index = FindCurrentCupIndex(current_cup, ring)
-		current_cup = ring[(current_cup_index+1)%len(ring)]
+		// need to store the value after the destination cup so
+		// we can set it as the NextNode to the last of the 3 cups
+		after_destination := destination_cup.NextNode
+		// set the next code after destination cup to the three cups in order
+		destination_cup.NextNode = next_three_cups
+		next_three_cups.NextNode.NextNode.NextNode = after_destination
+
+		// next cup should be (by rules) the next cup in line
+		current_cup = current_cup.NextNode
 
 		move++
-		if move%5 == 0 {
-			fmt.Println("on move:", move)
-		}
 	}
 
 	return ring
 }
 
 func day23() {
-	inp, _ := ioutil.ReadFile("./inputs/day23.sample")
+	inp, _ := ioutil.ReadFile("./inputs/day23.input")
 
 	data := GetStringInput(inp)
-	ring := []int{}
 
+	part2 := true
+	cache = make(map[int]*Node)
+
+	var previous_node *Node
+	var first_node *Node
+
+	numbers := []int{}
 	for _, k := range data[0] {
 		next_int, _ := strconv.Atoi(string(k))
-		ring = append(ring, next_int)
+		numbers = append(numbers, next_int)
 	}
 
-	final_ring := PlayCupsGame(append([]int{}, ring...), 100)
+	if part2 {
+		for i := 10; i <= 1_000_000; i++ {
+			numbers = append(numbers, i)
+		}
+	}
+	for _, k := range numbers {
+		size++
 
-	final_string := ""
-	one_index := FindCurrentCupIndex(1, final_ring)
-	for len(final_string) < 8 {
-		number := strconv.Itoa(final_ring[(one_index+1+len(final_string))%9])
-		final_string += number
+		node := Node{k, first_node}
+		cache[k] = &node
+
+		if first_node == nil {
+			first_node = &node
+			first_node.NextNode = &node
+		} else {
+			previous_node.NextNode = &node
+		}
+		previous_node = &node
 	}
 
-	fmt.Println("final string:", final_string)
-	i := 10
-	for i <= 1_000_000 {
-		ring = append(ring, i)
-		i++
+	total_moves := 100
+	if part2 {
+		total_moves = 10_000_000
 	}
-	fmt.Println("starting game")
-	final_ring = PlayCupsGame(append([]int{}, ring...), 10_000_000)
-	fmt.Println("starting game")
+	final_ring := PlayCupsGame(first_node, total_moves)
 
-	one_index = FindCurrentCupIndex(1, final_ring)
-	fmt.Println(final_ring[one_index+1], final_ring[one_index+2])
+	// find the cup with the label 1
+	one_node := final_ring
+	for {
+		if one_node.val == 1 {
+			break
+		}
+		one_node = one_node.NextNode
+	}
+
+	if part2 {
+		fmt.Println("final:", one_node, one_node.NextNode, one_node.NextNode.NextNode)
+		fmt.Println("prod:", one_node.NextNode.val*one_node.NextNode.NextNode.val)
+	} else {
+		final_string := GetRingString(one_node.NextNode, "")
+		final_string = final_string[:len(final_string)-1]
+		fmt.Println(final_string)
+	}
 }
